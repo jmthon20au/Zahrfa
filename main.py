0b8fd1e01d8d8a32deee753e94f580cb1a0c8218
jmthon20au <158489980+jmthon20au@users.noreply.github.com>
2024-02-13 14:25:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
 import requests
-import telebot
-from telebot import types
-bot = telebot.TeleBot("6589521510:AAH2XlLc63zd0-z4LA9fF-PjOarqPvlU-dY")
+from telebot.types import Message
+from telebot import TeleBot
+
+token = "6418845303:AAGV-jU1GiVv21Z44awdtN2f2ULwz_bkz2Q"
+bot = TeleBot(token)
+API = 'https://nsssar.tk/photo/photo.php'
+
 @bot.message_handler(commands=["start"])
-def startt(message):
-    start = types.InlineKeyboardButton(text="جلب المواقيت",callback_data="get")
-    start1 = types.InlineKeyboardButton(text="حسابي",url="https://t.me/B_xxBx")
-    btn1 = types.InlineKeyboardMarkup(row_width=1)
-    btn1.add(start,start1)
-    bot.reply_to(message ,"اهلًا بك في بوت مواقيت الصلاة حسب مدينتك اضغط على زر ( جلب المواقيت ) للبدأ",reply_markup=btn1)
-@bot.callback_query_handler(func =lambda call:True)
-def comm(call):
-    if call.data=="get":
-        back = types.InlineKeyboardButton(text="رجوع",callback_data="back")
-        btn2 = types.InlineKeyboardMarkup()
-        btn2.add(back)
-        v = bot.edit_message_text(chat_id=call.message.chat.id ,message_id =call.message.message_id ,text="اكتب اسم مدينتك بالعربي",reply_markup=btn2)
-        bot.register_next_step_handler(v,go)
-    elif call.data == "back":
-        start = types.InlineKeyboardButton(text="جلب المواقيت",callback_data="get")
-        start1 = types.InlineKeyboardButton(text="حسابي",url="https://t.me/B_xxBx")
-        btn1 = types.InlineKeyboardMarkup(row_width=1)
-        btn1.add(start,start1)
-        bot.edit_message_text(chat_id =call.message.chat.id ,message_id =call.message.message_id,text="اهلًا بك في بوت مواقيت الصلاة حسب مدينتك اضغط على زر ( جلب المواقيت ) للبدأ",reply_markup=btn1)
-    elif call.data == "ag":
-        start = types.InlineKeyboardButton(text="جلب المواقيت",callback_data="get")
-        start1 = types.InlineKeyboardButton(text="حسابي",url="https://t.me/B_xxBx")
-        btn1 = types.InlineKeyboardMarkup(row_width=1)
-        btn1.add(start,start1)
-        bot.edit_message_text(chat_id =call.message.chat.id ,message_id =call.message.message_id,text="اهلًا بك في بوت مواقيت الصلاة حسب مدينتك اضغط على زر ( جلب المواقيت ) للبدأ",reply_markup=btn1)
-def go(message):
-    btn3 = types.InlineKeyboardMarkup()
-    bacck = types.InlineKeyboardButton(text="ابدأ من جديد",callback_data="ag")
-    btn3.add(bacck)
-    cc = str(message.text)
-    req = requests.get(f"https://prayer.brhymibrahim1.repl.co/ibrahim?city={cc}").json()
-    date = req['main']['date']
-    city = req['main']['city']
-    fajr = req['main']['fajr']
-    shurooq = req['main']['shurooq']
-    dhuhr = req['main']['dhuhr']
-    asr = req['main']['asr']
-    maghrib = req['main']['maghrib']
-    isha = req['main']['isha']
-    all = f"-------------------------------\nالمدينة : {city}\nالتاريخ : {date}\nالفجر : {fajr}\nالشروق : {shurooq}\nالظهر : {dhuhr}\nالعصر : {asr}\nالمغرب : {maghrib}\nالعشاء : {isha}\n-------------------------------\n"
-    bot.reply_to(message ,all, reply_markup=btn3)
-bot.polling()
+def start(message: Message):
+    bot.reply_to(message, "Please send an image to convert to an anime.")
+
+@bot.message_handler(content_types=["photo"])
+def receiver(message: Message):
+    file_id = message.photo[2].file_id
+    file_url = bot.get_file_url(file_id)
+    params = {"almortagel": file_url}
+    response = requests.post(API, params=params).json()
+    bot.send_photo(chat_id=message.chat.id, photo=response["image"])
+
+print(bot.get_me().username)
+bot.infinity_polling()
